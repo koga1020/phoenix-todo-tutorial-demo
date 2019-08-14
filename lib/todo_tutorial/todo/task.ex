4e176924222bd021462df05b2c7ff29f78dee1b7
koga1020 <koga.shozo1020@gmail.com>
2019-08-14 16:00:18 +0900

    auto generate finished_at

--- lib/todo_tutorial/todo/task.ex
+++ lib/todo_tutorial/todo/task.ex
@@ -13,7 +13,18 @@ defmodule TodoTutorial.Todo.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:name, :is_finished, :finished_at])
-    |> validate_required([:name, :is_finished, :finished_at])
+    |> cast(attrs, [:name, :is_finished])
+    |> validate_required([:name, :is_finished])
+    |> put_finished_at_change()
   end
+
+  def put_finished_at_change(%Ecto.Changeset{changes: %{is_finished: true}} = changeset) do
+    now = %{NaiveDateTime.utc_now() | microsecond: {0, 0}}
+    put_change(changeset, :finished_at, now)
+  end
+  def put_finished_at_change(%Ecto.Changeset{changes: %{is_finished: false}} = changeset) do
+    put_change(changeset, :finished_at, nil)
+  end
+  def put_finished_at_change(changeset), do: changeset
+
 end
